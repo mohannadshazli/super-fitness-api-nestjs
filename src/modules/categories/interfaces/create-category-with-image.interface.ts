cb9d36543f156825382711085ad8553e5a7ce2ca
Mohannad Shazli <mohannadshazli@gmail.com>
2026-05-12 21:47:14 +0300

    Cleanup: remove modules re-introduced by PR merge

--- src/modules/categories/interfaces/create-category-with-image.interface.ts
+++
@@ -1,6 +0,0 @@
-import { CreateCategoryDto } from '../dto/create-category.dto';
-
-export interface ICreateCategoryWithImage extends CreateCategoryDto {
-  image_url: string;
-  image_public_id: string;
-}
--- src/modules/sub-categories/interfaces/create-sub-category-with-image.interface.ts
+++
@@ -1,6 +0,0 @@
-import { CreateSubCategoryDto } from '../dto/create-sub-category.dto';
-
-export interface ICreateSubCategoryWithImage extends CreateSubCategoryDto {
-  image_url?: string | null;
-  image_public_id?: string | null;
-}
